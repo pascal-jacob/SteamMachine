9007ac5a3aa5b064d77c9c8eb570b627d4658356
Pascal Jacob <pascal.jacob@globis-software.com>
2023-11-01 12:39:50 +0100

    Starting steam_machine

--- cdk/bin/cloud-gaming-on-ec2.ts
+++ cdk/bin/cloud-gaming-on-ec2.ts
@@ -13,12 +13,12 @@ const GRID_SW_CERT_URL = 'https://nvidia-gaming.s3.amazonaws.com/GridSwCert-Arch
 const CHROME_URL = 'https://dl.google.com/tag/s/appname=Google%20Chrome&needsadmin=true&ap=x64-stable-statsdef_0&brand=GCEA/dl/chrome/install/googlechromestandaloneenterprise64.msi';
 const SEVEN_ZIP_URL = 'https://www.7-zip.org/a/7z2201-x64.msi';
 
-const EC2_KEYPAIR_NAME = 'CHANGE_ME';
+const EC2_KEYPAIR_NAME = 'steam_machine';
 const VOLUME_SIZE_GIB = 200;
 const OPEN_PORTS = [8443];
 const ALLOW_INBOUND_CIDR = '0.0.0.0/0';
-const ACCOUNT_ID = 'CHANGE_ME';
-const REGION = 'eu-west-2';
+const ACCOUNT_ID = '708824551879';
+const REGION = 'eu-west-1';
 
 new G4DNStack(app, 'CloudGamingsOnG4DN', {
   niceDCVDisplayDriverUrl: NICE_DCV_DISPLAY_DRIVER_URL,
@@ -47,7 +47,7 @@ new G5Stack(app, 'CloudGamingOnG5', {
   sevenZipUrl: SEVEN_ZIP_URL,
   chromeUrl: CHROME_URL,
   gridSwCertUrl: GRID_SW_CERT_URL,
-  instanceSize: ec2.InstanceSize.XLARGE,
+  instanceSize: ec2.InstanceSize.XLARGE2,
   ec2KeyName: EC2_KEYPAIR_NAME,
   volumeSizeGiB: VOLUME_SIZE_GIB,
   openPorts: OPEN_PORTS,
